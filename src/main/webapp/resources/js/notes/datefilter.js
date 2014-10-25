e99fc0f420247da87657c8bb4ee21a8aa018d3b3
Karister <adam.arczynski@gmail.com>
2014-10-25 21:38:40 +0200

    cancel date filter - replace ajax with form submit

--- src/main/webapp/resources/js/notes/datefilter.js
+++ src/main/webapp/resources/js/notes/datefilter.js
@@ -1,18 +1,18 @@
 NotesDateFilterScripts = {
 	clearDateFilter: function() {
-//		$("#dateForm").find("input[name='date']" ).val("");
-//		$("#dateForm").submit();
+		$("#dateForm").find("input[name='date']" ).val("");
+		$("#dateForm").submit();
 		
-		var params = {
-			url: $("#dateForm").attr("action"),
-			data: {
-				date: ""
-			},
-			dataType: "html",
-			success: function(data) {$("html").html(data);},
-			type: "get"
-		};
-		$.ajax(params);
+//		var params = {
+//			url: $("#dateForm").attr("action"),
+//			data: {
+//				date: ""
+//			},
+//			dataType: "html",
+//			success: function(data) {$("html").html(data);},
+//			type: "get"
+//		};
+//		$.ajax(params);
 	},
 	
 	submitDateFilterForm: function() {
