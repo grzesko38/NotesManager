67a43972687f7488276d605664b3db1ba63c6d49
Karister <adam.arczynski@gmail.com>
2015-03-25 01:14:58 +0100

    submit topbar login form with enter - js

--- src/main/webapp/resources/js/login.js
+++ src/main/webapp/resources/js/login.js
@@ -9,9 +9,21 @@ LoginScripts = {
 			$("#topBarLogoutForm").submit();
 		});
 	},
+	bindSubmitWithEnter: function() {
+		$("#topBarLoginForm").each(function() {
+	        $(this).find("input").keypress(function(e) {
+	            if(e.which == 10 || e.which == 13) {
+	                this.form.submit();
+	            }
+	        });
+
+	        $(this).find('input[type=submit]').hide();
+	    });
+	},
 	bindAll: function() {
 		LoginScripts.bindLogin();
 		LoginScripts.bindLogout();
+		LoginScripts.bindSubmitWithEnter();
 	}
 }
 
