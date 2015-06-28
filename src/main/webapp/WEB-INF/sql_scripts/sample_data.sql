c5c61a3febec11dbb38c22475bb1700861c8677d
Karister <adam.arczynski@gmail.com>
2015-06-28 16:31:09 +0200

    sql sample data

--- src/main/webapp/WEB-INF/sql_scripts/sample_data.sql
+++ src/main/webapp/WEB-INF/sql_scripts/sample_data.sql
@@ -4,7 +4,22 @@ INSERT INTO USERS
 )
 VALUES
 (
-	1, 'Adam', 'adam.arczynski@gmail.com', '1990-01-01 00:00:00', '6a8e7f5f2b4ebf1a219d551e46e1338cf149ef175d48de64e33322b5704963b4', 'sha-256', '25ffed', 1, 'RegisteredUser'
+	1, 'Adam', 'adam.arczynski@gmail.com', '1990-01-01 00:00:00', '0264abd1fc2a47a9d938201ae4429594882fb9887c8b7b3e8041c9784f8ac85c', 'sha-256', 'cfd6a7346064d78b38d9a5a2f19f2963', 1, 'RegisteredUser'
+),
+(
+	2, 'Adam', NULL, NULL, NULL, NULL, NULL, NULL, 'AnonymousUser'
+),
+(
+	3, 'Olka', NULL, NULL, NULL, NULL, NULL, NULL, 'AnonymousUser'
+),
+(
+	4, 'Alka', NULL, NULL, NULL, NULL, NULL, NULL, 'AnonymousUser'
+),
+(
+	5, 'Misiek', NULL, NULL, NULL, NULL, NULL, NULL, 'AnonymousUser'
+),
+(
+	6, 'Witek', NULL, NULL, NULL, NULL, NULL, NULL, 'AnonymousUser'
 );
 
 INSERT INTO USER_ROLES
@@ -17,6 +32,21 @@ VALUES
 ),
 (
 	2, 1, 'ROLE_ADMIN'
+),
+(
+	3, 2, 'ROLE_ANONYMOUS'
+),
+(
+	4, 3, 'ROLE_ANONYMOUS'
+),
+(
+	5, 4, 'ROLE_ANONYMOUS'
+),
+(
+	6, 5, 'ROLE_ANONYMOUS'
+),
+(
+	7, 6, 'ROLE_ANONYMOUS'
 );
 
 INSERT INTO USER_PREFERENCES
@@ -30,45 +60,60 @@ VALUES
 
 INSERT INTO NOTES
 (
-	user_fk, date_created, content
+	user_fk, date_created, last_modified, deadline, title, content, latitude, longitude
 )
 VALUES
 (
-	1, '2008-10-05 00:00:00', 'moja notatka ble ble ble'
+	1, '2008-10-05 00:00:00', NULL, '2008-10-25 00:00:00', 'Wpis 123 abc', 'moja notatka ble ble ble ble bla bla', 50.4822855, 17.32958610000003
 ),
 (
-	1, '2014-03-04 00:00:00', 'moja notatka ble ble ble'
+	1, '2008-11-07 00:00:00', NULL, '2011-10-25 00:00:00', 'Wpis 123 abc', 'wpis aaa bbb', 50.4822855, 17.32958610000003
 ),
 (
-	1, '2011-05-25 00:00:00', 'moja notatka 2 bla bla'
+	2, '2009-01-01 00:00:00', NULL, '2010-10-30 00:00:00', 'Wpis Adama', 'wpis aaa bbb', 50.4822855, 17.32958610000003
 ),
 (
-	1, '1997-01-01 00:00:00', 'wpis aaa'
+	2, '2005-01-01 00:00:00', NULL, '2006-05-23 00:00:00', 'Adam Adam Adam', 'wpis aaa bbb text text text text abc 123 123', 50.4822855, 17.32958610000003
 ),
 (
-	1, '1998-10-12 00:00:00', 'wpis aaa 2'
+	2, '2009-01-01 00:00:00', NULL, '2011-10-30 00:00:00', 'XSS hax0R', '<script>alert("hacked!")</script>', 51.1329635, 17.065819000000033
 ),
 (
-	1, '2013-08-22 00:00:00', 'rey rey rey rey rey'
+	2, '1999-01-01 00:00:00', NULL, '2001-10-30 00:00:00', 'Life in Texas', 'Texas 1234566789', 31.9685988, -99.90181310000003
 ),
 (
-	1, '2013-04-17 00:00:00', 'and and and and and and'
+	2, '2009-01-01 00:00:00', NULL, '2011-10-30 00:00:00', 'test no map attached', 'ble ble', NULL, NULL
 ),
 (
-	1, '2007-02-04 00:00:00', 'ilona ilona ilona ilona ilona'
+	3, '2009-04-01 00:00:00', NULL, '2015-10-30 00:00:00', 'notka Olki', 'xxxx cccc vvvvvv dddd fffffff dddd yyy', NULL, NULL
 ),
 (
-	1, '2011-10-30 00:00:00', 'jjjj jjjjjjjjjjjjj jjj jjjjjjjjjj j jj'
+	3, '2012-04-01 00:00:00', NULL, '2016-10-30 00:00:00', 'Blizzard Entertaiment, Irvine', 'World of Warcraft', 33.658161, -117.767091
 ),
 (
-	1, '2005-04-18 00:00:00', 'Maciek Maciek Maciek Maciek Maciek'
+	3, '2010-09-07 00:00:00', NULL, '2011-05-14 00:00:00', 'Wawel', 'Kraków', 50.06465009999999, 19.94497990000002
 ),
 (
-	1, '2000-01-01 00:00:00', 'rererererer erererere ererere'
+	4, '2009-03-07 00:00:00', NULL, '2014-10-30 00:00:00', 'Ala ma kota', 'tttteeeeeeeeeexxxxxxxxxxxxeeeeettttt mooooreee teeexxxtttt', 50.06465009999999, 19.94497990000002
 ),
 (
-	1, '2004-05-21 00:00:00', 'ola ola ola ola'
+	5, '2005-03-11 00:00:00', NULL, '2005-05-14 00:00:00', 'Miś Koala', 'brrrrrrr brrr', 73.03757499999999, -85.148031
 ),
 (
-	1, '2014-12-01 00:00:00', 'o2o2o2o2o2o2'
+	5, '2004-04-07 00:00:00', NULL, '2012-12-14 00:00:00', 'HTML test', 'test <b>bold</b>', 73.03757499999999, -85.148031
+),
+(
+	5, '2003-04-07 00:00:00', NULL, '2003-05-21 00:00:00', 'gg teeeesst gggg', 'hhhhh hhhhh hhhhh', NULL, NULL
+),
+(
+	5, '2011-04-07 00:00:00', NULL, '2013-03-08 00:00:00', 'Dzień kobiet', '8. marca', 50.662814, 17.919937000000004
+),
+(
+	6, '2001-04-07 00:00:00', NULL, '2008-12-08 00:00:00', 'Morze', 'Kołobrzeg', 54.17591729999999, 15.583266699999967
+),
+(
+	6, '2011-04-07 00:00:00', NULL, '2011-10-10 00:00:00', 'Urodzinki', 'Urodziny Tomka', NULL, NULL
+),
+(
+	6, '2010-04-07 00:00:00', NULL, '2010-09-08 00:00:00', 'pwr wiz studia', 'w8 wiz', 51.108673, 17.060158
 );
